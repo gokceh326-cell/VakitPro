a01b4db779463d51710517c576ef04e02ab22b7e
gokceh326-cell <gokceh326@gmail.com>
2026-08-25 02:07:21 +0300

    Update MainActivity.java

--- app/src/main/java/com/vakitpro/MainActivity.java
+++ app/src/main/java/com/vakitpro/MainActivity.java
@@ -2,6 +2,7 @@ package com.vakitpro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,13 +15,20 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         webView = new WebView(this);
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-
         setContentView(webView);
 
+        WebSettings settings = webView.getSettings();
+
+        // JavaScript'i aç
+        settings.setJavaScriptEnabled(true);
+
+        // HTML içindeki localStorage vb. özellikler için
+        settings.setDomStorageEnabled(true);
+
+        // Sayfaların WebView içinde açılması
+        webView.setWebViewClient(new WebViewClient());
+
+        // VakitPro HTML sayfasını aç
         webView.loadUrl("file:///android_asset/index.html");
     }
 
